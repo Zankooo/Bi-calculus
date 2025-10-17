8c2fe16d90114ce3065aabcefee116711340fc00
Zankooo <stankovic.zan@gmail.com>
2025-10-17 15:19:12 +0200

    fixes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,11 @@
 import { Controller, Get, Res } from '@nestjs/common';
-import type { Response } from 'express';
-import { join } from 'path';
+
 
 @Controller()
 export class AppController {
   @Get('hello')
   getHello() {
-    return { message: 'Hello World' };
+    return { message: 'Hello World!!!' };
   }
 
   
